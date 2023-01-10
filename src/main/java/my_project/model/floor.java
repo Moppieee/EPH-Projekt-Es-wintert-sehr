3c9ed040d84a66ea2b25e1d65e5daff1fc8d5bf1
troxisai <troxisai@kgg-do.eu>
2023-01-10 10:21:26 +0100

    floor

---
+++ src/main/java/my_project/model/floor.java
@@ -0,0 +1,37 @@
+package my_project.model;
+
+import KAGO_framework.model.GraphicalObject;
+import KAGO_framework.view.DrawTool;
+
+import java.awt.*;
+
+
+public class floor extends GraphicalObject {
+
+    /**
+
+     * @param x Startposition x
+     * @param y Startposition y
+     */
+    public floor(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void draw(DrawTool drawTool) {
+        drawTool.setCurrentColor(Color.GREEN);
+        drawTool.drawFilledRectangle(x,y,10,10);
+    }
+
+    /**
+     * Wird mit jedem Frame vom Framework aufgerufen und dient zur Manipulation des Objekts im Verlauf
+     * der Zeit.
+     * @param dt die Sekunden, die seit dem letzten Aufruf von update vergangen sind
+     */
+    @Override
+    public void update(double dt){
+
+    }
+
+}
